c69e96fbad448043e71ef364e0d8f22672a6a7ff
Adamo Morone <adamo.morone@gmail.com>
2019-09-16 23:13:31 -0300

    Adding marshmallow

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
-from resources.item import Item, ItemList
-from resources.store import Store, StoreList
 from blacklist import BLACKLIST
 
 app = Flask(__name__)
@@ -34,10 +32,6 @@ def check_if_token_in_blacklist(decrypted_token):
     return decrypted_token["jti"] in BLACKLIST
 
 
-api.add_resource(Store, "/store/<string:name>")
-api.add_resource(Item, "/item/<string:name>")
-api.add_resource(ItemList, "/items")
-api.add_resource(StoreList, "/stores")
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/user/<int:user_id>")
 api.add_resource(UserLogin, "/login")
--- models/user.py
+++ models/user.py
@@ -16,9 +16,6 @@ class UserModel(db.Model):
         self.username = username
         self.password = password
 
-    def json(self) -> UserJSON:
-        return {"id": self.id, "username": self.username}
-
     def save_to_db(self) -> None:
         db.session.add(self)
         db.session.commit()
