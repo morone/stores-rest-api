efb63ecaf8283a57ffdf8ac7f38e1be26985daae
Adamo Morone <adamo.morone@gmail.com>
2019-09-16 23:30:33 -0300

    Marshmallow

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
+from ma import ma
 from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from blacklist import BLACKLIST
 
@@ -43,4 +44,5 @@ if __name__ == "__main__":
     from db import db
 
     db.init_app(app)
+    ma.init_app(app)
     app.run(port=5000, debug=True)
--- resources/user.py
+++ resources/user.py
@@ -31,13 +31,12 @@ class UserRegister(Resource):
     @classmethod
     def post(cls):
         try:
-            user_data = user_schema.load(request.get_json())
+            user = user_schema.load(request.get_json())
         except ValidationError as err:
             return err.messages, 400
-        if UserModel.find_by_username(user_data["username"]):
+        if UserModel.find_by_username(user.username):
             return {"message": USER_ALREADY_EXISTS}, 400
 
-        user = UserModel(**user_data)
         user.save_to_db()
 
         return {"message": CREATED_SUCCESSFULLY}, 201
@@ -67,9 +66,9 @@ class UserLogin(Resource):
             user_data = user_schema.load(request.get_json())
         except ValidationError as err:
             return err.messages, 400
-        user = UserModel.find_by_username(user_data["username"])
+        user = UserModel.find_by_username(user_data.username)
 
-        if user and safe_str_cmp(user.password, user_data["password"]):
+        if user and safe_str_cmp(user.password, user_data.password):
             access_token = create_access_token(identity=user.id, fresh=True)
             refresh_token = create_refresh_token(user.id)
             return {"access_token": access_token, "refresh_token": refresh_token}, 200
