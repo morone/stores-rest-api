8bcc15e8e7011ceca8595a7f746b88bb9c42a8d7
Adamo Morone <adamo.morone@gmail.com>
2019-09-16 22:02:34 -0300

    Adding hinting
    Formating code

--- app.py
+++ app.py
@@ -10,77 +10,43 @@ from resources.store import Store, StoreList
 from blacklist import BLACKLIST
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['JWT_BLACKLIST_ENABLED'] = True
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
-app.secret_key = 'adamo'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "sqlite:///data.db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["JWT_BLACKLIST_ENABLED"] = True
+app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access", "refresh"]
+app.secret_key = "adamo"
 api = Api(app)
+
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+
 jwt = JWTManager(app)
 
-@jwt.user_claims_loader
-def add_clains_to_jwt(identity):
-    if identity == 1:
-        return {'is_admin': True}
-    return {'is_admin': False}
 
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
-    return decrypted_token['jti'] in BLACKLIST
-
-@jwt.expired_token_loader
-def expired_token_callback():
-    return jsonify({
-        'description': 'The token has expired',
-        'error': 'token_expired'
-    }), 401
-
-@jwt.invalid_token_loader
-def invalid_token_callback(error):
-    return jsonify({
-        'description': 'What are you doiing?',
-        'error': 'invalid_token'
-    }),401
-
-@jwt.unauthorized_loader
-def unauthorized_token_callback(error):
-    return jsonify({
-        'description': 'What are you doiing?',
-        'error': 'unauthorized_token'
-    }),401
-
-@jwt.needs_fresh_token_loader
-def token_not_refresh_callback(error):
-    return jsonify({
-        'description': 'What are you doiing?',
-        'error': 'token_not_refreshed'
-    }),401
-
-@jwt.revoked_token_loader
-def revoked_token_callback(error):
-    return jsonify({
-        'description': 'What are you doiing?',
-        'error': 'revoked_token'
-    }),401
-
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
-api.add_resource(UserRegister, '/register')
-api.add_resource(User, '/user/<int:user_id>')
-api.add_resource(UserLogin, '/login')
-api.add_resource(UserLogout, '/logout')
-api.add_resource(TokenRefresh, '/refresh')
+    return decrypted_token["jti"] in BLACKLIST
 
 
-if __name__ == '__main__':
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(StoreList, "/stores")
+api.add_resource(UserRegister, "/register")
+api.add_resource(User, "/user/<int:user_id>")
+api.add_resource(UserLogin, "/login")
+api.add_resource(UserLogout, "/logout")
+api.add_resource(TokenRefresh, "/refresh")
+
+
+if __name__ == "__main__":
     from db import db
+
     db.init_app(app)
     app.run(port=5000, debug=True)
--- resources/store.py
+++ resources/store.py
@@ -1,32 +1,38 @@
+from typing import Dict, List
+
 from flask_restful import Resource
 from models.store import StoreModel
 
+
 class Store(Resource):
-    def get(self, name):
+    def get(self, name: str):
         store = StoreModel.find_by_name(name)
         if store:
             return store.json()
-        return {'message': 'Store not found'}, 404
+        return {"message": "Store not found"}, 404
 
-    def post(self, name):
+    def post(self, name: str):
         if StoreModel.find_by_name(name):
-            return {"message': 'A store with name '{}' already exists.".format(name)}, 400
+            return (
+                {"message': 'A store with name '{}' already exists.".format(name)},
+                400,
+            )
 
         store = StoreModel(name)
         try:
             store.save_to_db()
         except:
-            return {'message': 'An error occurred while creating the store.'}, 500
-        
+            return {"message": "An error occurred while creating the store."}, 500
+
         return store.json(), 201
 
-    def delete(self, name):
-        if  StoreModel.find_by_name(name):
+    def delete(self, name: str):
+        if StoreModel.find_by_name(name):
             store.delete_from_db()
-        
-        return {'message': 'Store deleted'}
+
+        return {"message": "Store deleted"}
 
 
 class StoreList(Resource):
     def get(self):
-        return {'stores': [store.json() for store in StoreModel.find_all()]}
+        return {"stores": [store.json() for store in StoreModel.find_all()]}
